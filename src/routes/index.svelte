b8b897d13548163c4e03f0524a91f7c15edefe43
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-20 10:22:59 +0800

    Lesson 3: Svelte Components (refresh)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'Ninja Gaming Guides';
+
+    const updateTitle = () => {
+        title = 'Something Completely Different'
+    }
+</script>
+
+
+<div class="index">
+    <h1>
+        {#if title !== ''}
+            { title }
+        {:else}
+            &nbsp;
+        {/if}
+    </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cupiditate ea esse quo. Consectetur dolorum eius eos harum pariatur perferendis? Assumenda aut, exercitationem laboriosam obcaecati quaerat quia recusandae sed voluptatum.</p>
+
+    <button on:click={ updateTitle }>Change Title</button>
+    <input type="text" bind:value={ title }>
+</div>
+
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
